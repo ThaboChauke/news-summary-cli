51d14e220286f640ecf97d76bbe02fda933dc9e7
ThaboChauke <chaukethabo16@gmail.com>
2024-11-23 19:51:58 +0200

    display headlines and links

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ import requests
 from dotenv import load_dotenv
 
 load_dotenv()
-
 news_api_key = os.getenv("NEWS")
-
 
 def get_news(api_key):
     url = f"https://newsdata.io/api/1/latest?apikey={api_key}&q=southafrica"
     return requests.get(url)
-
 
 def process_news(news):
     headlines = []
@@ -20,12 +17,18 @@ def process_news(news):
 
     return headlines
 
-
 def main():
     news = get_news(news_api_key)
-    process_news(news)
+    headlines = process_news(news)
 
+    print("\n---------------------------->Latest Headlines<----------------------------\n")
 
+    for headline in enumerate(headlines, start=1):
+
+        print(headline[0] , ". HEADLINE: " + headline[1].get("title") + "\n"
+              + "LINK TO ARTICLE: " + headline[1].get("url") + "\n")
+
+    print("\n---------------------------->END<----------------------------\n")
 
 if __name__ == "__main__":
     main()
