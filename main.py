0aa8e33af8217ea4c8691b7e86f1bf83bea46577
ThaboChauke <chaukethabo16@gmail.com>
2024-11-23 19:07:16 +0200

    get news function

--- main.py
+++ main.py
@@ -7,6 +7,25 @@ load_dotenv()
 news_api_key = os.getenv("NEWS")
 
 
-URL = f"https://newsdata.io/api/1/latest?apikey={news_api_key}&q=southafrica"
-response = requests.get(URL)
-print(response.json())
+def get_news(api_key):
+    url = f"https://newsdata.io/api/1/latest?apikey={api_key}&q=southafrica"
+    return requests.get(url)
+
+
+def process_news(news):
+
+    results = news.json().get("results")
+
+
+
+    pass
+
+
+def main():
+    news = get_news(news_api_key)
+    process_news(news)
+
+
+
+if __name__ == "__main__":
+    main()
